c1e2318c52cad77284244c199574579dabd08e9b
StanVSV <stanislav2913vaivalov@gmail.com>
2024-02-21 20:57:00 +0100

    Update

---
+++ js/task-2.js
@@ -0,0 +1,77 @@
+// function each(array, callback) {
+//     const newArr = [];
+//     for (const item of array) {
+//         const res = callback(item);
+//         newArr.push(res)
+//     }
+//     return newArr;
+// }
+
+// console.log(each([64, 48, 25, 9, 16], function (value) {
+//     return value * 2;
+// }));
+
+// console.log(each([64, 48, 25, 9, 16], function (value) {
+//     return value -10;
+// }));
+
+// console.log(each([64, 48, 25, 9, 16], function (value) {
+//     return Math.sqrt(value);
+// }));
+
+
+
+// const foo = a => {
+//     return a + 5;
+// }
+
+// console.log(foo(2, 3));
+
+
+
+
+// const foo = () => ({a: 5})
+
+// console.log(foo());
+
+
+
+// const array = [5, 10, 15, 20, 25];
+
+// array.forEach(function(item, index, arr) {
+//     console.log('item', item);
+//     console.log('index', index);
+//     console.log('arr', arr);
+// });
+
+
+
+// let total = 0;
+
+// array.forEach(item => {
+//     total += item;
+// })
+
+// console.log(total);
+
+
+
+
+// const calculateAverage = (...args) => {
+//     let total = 0;
+//     args.forEach(item => {
+//         total += item
+//     })
+//     return 
+// }
+
+
+
+// const getModels = cars => {
+//     return cars.map(item => item.modal);
+// };
+
+// console.log(getModels(allCars));
+
+
+
